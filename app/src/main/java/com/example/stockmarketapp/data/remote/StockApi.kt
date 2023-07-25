02eab39021c37a91f2c49bb8f744e669dafc2c47
Saurabh <saurabhm1309@gmail.com>
2023-07-25 15:23:53 +0530

    API and DB setup

---
+++ app/src/main/java/com/example/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,21 @@
+package com.example.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListing(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_KEY = "IK0WBHK9RZSL85SK"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+
+}
