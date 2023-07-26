3b6db577dc26b7d8be11b61e600dc36b28298581
Saurabh <saurabhm1309@gmail.com>
2023-07-26 16:35:55 +0530

    Company Listing Screen + DI

--- app/src/main/java/com/example/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/example/stockmarketapp/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph
+import com.example.stockmarketapp.presentation.company_listing.NavGraphs
 import com.example.stockmarketapp.ui.theme.StockMarketAppTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
